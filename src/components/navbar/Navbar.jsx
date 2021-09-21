cfd7c06b58ae7e81c1f17c36441f27d28aabd090
Elise Burridge <emjburridge@gmail.com>
2021-09-21 19:06:29 +0100

    added navbar icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import "./navbar.scss"
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 export default function Navbar() {
   return (
     <div className="navbar">
-
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">eriisu</a>
+          <div className="itemContainer">
+            <MailOutlineIcon className="icon"/>
+            <span>emjburridge@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
