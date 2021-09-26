cf0c3662a18bbc68d9f0f5e11a7773645de96ee0
Elise Burridge <emjburridge@gmail.com>
2021-09-26 18:10:59 +0100

    added logo and changed navbar fade animations

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import "./navbar.scss"
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import logoBlack from "../../assets/logo_black.png";
+import logoWhite from "../../assets/logo_white.png";
 
 export default function Navbar( { menuOpen, setMenuOpen } ) {
   return (
     <div className={"navbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo">eriisu</a>
+          <a href="#intro" className="logo"> <img src={logoBlack} alt="logo" className="logo"/> </a>
+          <a href="#intro" className="logoActive"> <img src={logoWhite} alt="logoActive" className="logoActive"/> </a>
           <div className="itemContainer">
             <MailOutlineIcon className="icon"/>
             <span>emjburridge@gmail.com</span>
